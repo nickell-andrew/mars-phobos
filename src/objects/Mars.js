6d575399285946b4715bf3ae39ecdc68bf694c25
nickell-andrew <nickell.andrew@gmail.com>
2015-04-28 15:36:29 -0700

    updated Mars to ES6

--- src/objects/Mars.js
+++ src/objects/Mars.js
@@ -1,29 +1,33 @@
 import THREE from 'three';
 import Physics from '../ngraph-physics.js';
   
-function Mars(){
-  THREE.Object3D.call(this);
-  var radius = 0.08;
-  var texture = new THREE.ImageUtils.loadTexture('images/textures/marsmap2k.jpg');
-  var bumpMap = new THREE.ImageUtils.loadTexture('images/textures/marsbump2k.jpg');
-  var geometry = new THREE.SphereGeometry(radius, 100, 100);
-  var material = new THREE.MeshPhongMaterial({
-    color: 0x552222, 
-    map: texture,
-    bumpMap: bumpMap,
-    bumpScale: .02,
-    shininess: .000000001
-  });
-  this.mesh = new THREE.Mesh(geometry, material);
-  this.add(this.mesh);
-  this.particle = Physics.createParticle({x:0,y:0,z:0}, radius);
+class Mars extends THREE.Object3D {
+  constructor() {
+    //THREE.Object3D.call(this);
+    super();
+    var radius = 0.08;
+    var texture = new THREE.ImageUtils.loadTexture('images/textures/marsmap2k.jpg');
+    var bumpMap = new THREE.ImageUtils.loadTexture('images/textures/marsbump2k.jpg');
+    var geometry = new THREE.SphereGeometry(radius, 100, 100);
+    var material = new THREE.MeshPhongMaterial({
+      color: 0x552222, 
+      map: texture,
+      bumpMap: bumpMap,
+      bumpScale: .02,
+      shininess: .000000001
+    });
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.add(this.mesh);
+    this.particle = Physics.createParticle({x:0,y:0,z:0}, radius);
+  }
+
+
+/*Mars.prototype = new THREE.Object3D;
+Mars.prototype.constructor = Mars; */
+
+  update() {
+    this.mesh.rotation.y += 0.01;
+  }
 }
-
-Mars.prototype = new THREE.Object3D;
-Mars.prototype.constructor = Mars;
-
-Mars.prototype.update = function() {
-  this.mesh.rotation.y += 0.01;
-};
 
 export default Mars;
