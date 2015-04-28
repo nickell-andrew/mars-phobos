41ca720ad7595d230f8027ab0fc42d384502203c
nickell-andrew <nickell.andrew@gmail.com>
2015-04-28 16:26:16 -0700

    Moon extends CelestialBody and redundancies removed

--- src/objects/Moon.js
+++ src/objects/Moon.js
@@ -1,14 +1,13 @@
 import THREE from 'three';
 import Physics from '../ngraph-physics.js';
+import CelestialBody from './CelestialBody';
 
 
-class Moon extends THREE.Object3D {
+class Moon extends CelestialBody {
   constructor() {
-    super();
     var radius = 0.02;
     var texture = new THREE.ImageUtils.loadTexture('images/textures/moonmap2k.jpg');
     var bumpMap = new THREE.ImageUtils.loadTexture('images/textures/moonbump2k.jpg');
-    var geometry = new THREE.SphereGeometry(radius, 100, 100);
     var material = new THREE.MeshPhongMaterial({
       color: 0x444444, 
       map: texture,
@@ -16,9 +15,7 @@ class Moon extends THREE.Object3D {
       bumpScale: .000001,
       shininess: .000000000001
     });
-    this.mesh = new THREE.Mesh(geometry, material);
-    this.add(this.mesh);
-    this.particle = Physics.createParticle({x:0,y:0,z:0}, radius);
+    super(radius, material);
     this.particle.body.velocity.y = 1.0;
   }
   update() {
